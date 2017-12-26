694dbca6774339edab1b8f63f645732e1adbb097
mofm <emre.eryilmaz@piesso.com>
2017-12-27 00:50:31 +0300

    add geoip country address

--- wtfip.py
+++ wtfip.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import pygeoip
 
 app = Flask(__name__)
-
+gi = pygeoip.GeoIP('data/GeoLiteCity.dat', pygeoip.MEMORY_CACHE)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    geo_data = gi.record_by_addr(request.remote_addr)
+    return render_template('home.html', geo_data=geo_data)
 
 
 if __name__ == '__main__':
